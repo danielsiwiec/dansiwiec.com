3986bc1b9ea70c9dda2112cc97227873a1fd5857
Dan Siwiec <daniel.siwiec@gmail.com>
2018-09-26 13:13:57 -0700

    Add favicon

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -2,13 +2,19 @@ import React from 'react'
 import Helmet from 'react-helmet'
 import '../../assets/scss/init.scss'
 import 'prismjs/themes/prism.css'
+import favicon from '../../../static/favicon.png'
 
 export default props => {
   const { children } = props
 
   return (
     <div className='layout'>
-      <Helmet defaultTitle='Blog by Dan Siwiec' />
+      <Helmet
+        defaultTitle='Blog by Dan Siwiec'
+        link={[
+          { rel: 'icon', type: 'image/png', href: `${favicon}` }
+        ]}
+      />
       {children}
     </div>
   )
