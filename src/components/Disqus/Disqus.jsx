9f673b405010040fa1f20089b6f7899710b0c948
Dan Siwiec <daniel.siwiec@gmail.com>
2018-11-12 16:28:09 -0800

    Replace disqus custom module with official one

--- src/components/Disqus/Disqus.jsx
+++ src/components/Disqus/Disqus.jsx
@@ -1,39 +1,17 @@
 import React, { Component } from 'react'
-import ReactDisqusComments from 'react-disqus-comments'
+import { DiscussionEmbed } from 'disqus-react'
 
 class Disqus extends Component {
-  constructor (props) {
-    super(props)
-    this.state = { toasts: [] }
-    this.notifyAboutComment = this.notifyAboutComment.bind(this)
-    this.onSnackbarDismiss = this.onSnackbarDismiss.bind(this)
-  }
-
-  onSnackbarDismiss () {
-    const [, ...toasts] = this.state.toasts
-    this.setState({ toasts })
-  }
-  notifyAboutComment () {
-    const toasts = this.state.toasts.slice()
-    toasts.push({ text: 'New comment available!!' })
-    this.setState({ toasts })
-  }
   render () {
     const { postNode, siteMetadata } = this.props
-    if (!siteMetadata.disqusShortname) {
-      return null
+    const disqusShortname = siteMetadata.disqusShortname
+    const disqusConfig = {
+      identifier: postNode.id,
+      title: postNode.frontmatter.title
     }
-    const post = postNode.frontmatter
-    const url = siteMetadata.url + postNode.fields.slug
+
     return (
-      <ReactDisqusComments
-        shortname={siteMetadata.disqusShortname}
-        identifier={post.title}
-        title={post.title}
-        url={url}
-        category_id={post.category_id}
-        onNewComment={this.notifyAboutComment}
-      />
+      <DiscussionEmbed shortname={disqusShortname} config={disqusConfig} />
     )
   }
 }
